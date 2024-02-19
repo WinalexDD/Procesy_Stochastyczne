f065ce1019d36fdba74e0e138375141ee927a705
WinalexDD <146812784+winalexdd@users.noreply.github.com>
2024-02-19 22:44:39 +0100

    Layout corrections

--- Run.py
+++ Run.py
@@ -1,28 +1,30 @@
 import os
-from B import podpunktB
 import matplotlib.pyplot as plt
+from B import podpunktB
 
-#Path
-dir=input("Prosze podać ścieżke z katalogiem healthy_decades: ")
-os.chdir(str(dir))
+# Inputting path to folder with data
+datapath = input("Prosze podać ścieżke z katalogiem healthy_decades: ")
+os.chdir(str(datapath))
 
-#List of files
+# Getting the list of individual files from chosen decade
 path = os.getcwd()
 dir_list = os.listdir(path)
-dekadaM=[]
-dekadaF=[]
-for i in range (0, len(dir_list)):
-    if 'm70' in dir_list[i] :
+dekadaM, dekadaF = [], []
+for i in range(0, len(dir_list)):
+    if 'm70' in dir_list[i]:
         dekadaM.append(dir_list[i])
     elif 'f70' in dir_list[i]:
         dekadaF.append(dir_list[i])
 
-#B
-df=podpunktB(dekadaF, dekadaM)
+# Initializing point B and
+df = podpunktB(dekadaF, dekadaM)
+
+# Creating folder to save results
 if not os.path.isdir("Dekada 70 - kobiety a mężczyzni"):
     os.mkdir("Dekada 70 - kobiety a mężczyzni")
 os.chdir("Dekada 70 - kobiety a mężczyzni")
 
+# Saving the table to .png file
 plt.rcParams["figure.figsize"] = (3, 3)
 fig, ax = plt.subplots()
 fig.patch.set_visible(False)
@@ -33,5 +35,4 @@ tabelawykres.set_fontsize(50)
 plt.gcf().set_size_inches(50, 50)
 plt.savefig("Kobiety a mężczyzni tabela.png")
 plt.close()
-
 os.chdir('..')
